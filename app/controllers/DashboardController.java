a2245125abef3646d2b6cf846874250df2c5562b
pravindaperera <pravinda.perera@syscolabs.com>
2018-11-20 20:00:52 +0530

    play project structure | initial views

---
+++ app/controllers/DashboardController.java
@@ -0,0 +1,73 @@
+package controllers;
+
+import play.mvc.*;
+
+import views.html.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class DashboardController extends Controller {
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result dashboard() {
+        return ok(dashboard.render("Library Manager - Dashboard"));
+    }
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result add() {
+        return ok(addItem.render("Library Manager - Add Item Section"));
+    }
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result delete() {
+        return ok(deleteItem.render("Library Manager - Delete Item Section"));
+    }
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result borrow() {
+        return ok(borrowItem.render("Library Manager - Borrow Item Section"));
+    }
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result returnItem() {
+        return ok(returnItem.render("Library Manager - Return Item Section"));
+    }
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result report() {
+        return ok(report.render("Library Manager - Report Generation Section"));
+    }
+
+}
--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := """play-java-starter-example"""
+name := """Library Manager"""
 
 version := "1.0-SNAPSHOT"
 
